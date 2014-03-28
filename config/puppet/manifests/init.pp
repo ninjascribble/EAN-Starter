f4599436926664e2ebd5ebd6ece541ecf3aa61e1
Scott Grogan <scott@ninjascript.com>
2014-03-27 21:18:40 -0700

    Adding bower to provisioner... but it doesn't work :(

--- config/puppet/manifests/init.pp
+++ config/puppet/manifests/init.pp
@@ -7,7 +7,8 @@ package { [
     'curl',
     'vim',
     'g++',
-    'make'
+    'make',
+    'git'
     ]:
     ensure => installed,
     require => Exec['apt-get_update']
@@ -20,28 +21,49 @@ class { 'nodejs':
 
 package { [
     'pm2',
-    'express'
+    'express',
+    'bower'
     ]:
     ensure => present,
     provider => 'npm',
     require => [
         Class['nodejs'],
-        Package['make']
+        Package['make'],
+        Package['git']
     ]
 }
 
-exec { 'install_app_dependencies':
+exec { 'install_npm_packages':
     command => 'npm install',
     path => '/usr/bin',
     cwd => '/var/src/app',
     require => Class['nodejs']
 }
 
+file{ 'init_bower_directory':
+    name => "/var/src/app/public/bower_components",
+    purge => true,
+    recurse => true,
+    force => true,
+    backup => false
+}
+
+exec { 'install_bower_packages':
+    command => 'bower install --verbose --config.interactive=false',
+    path => '/usr/bin',
+    cwd => '/var/src/app',
+    require => [
+        File['init_bower_directory'],
+        Package['bower']
+    ]
+}
+
 exec { 'auto_start_app':
     command => 'pm2 start /var/src/app/app.js -i max',
     path => '/usr/bin',
     require => [
-        Exec['install_app_dependencies'],
+        Exec['install_npm_packages'],
+        Exec['install_bower_packages'],
         Package['pm2']
     ]
 }
