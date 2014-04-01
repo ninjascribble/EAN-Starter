60907c3b5ab35cb517746e6a912c0a23863f7026
Scott Grogan <scott@ninjascript.com>
2014-03-31 20:34:13 -0700

    Replacing jade with handlebars and adding h5bp, 404 page

--- app/app.js
+++ app/app.js
@@ -1,20 +1,19 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-
+var exphbs = require('express3-handlebars');
 var app = express();
 
-// all environments
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main',
+	layoutsDir: path.join(__dirname, 'views/layouts'),
+	partialsDir: path.join(__dirname, 'views/partials')
+}));
+
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'handlebars');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -23,13 +22,12 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('*/*', routes.fourohfour);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
